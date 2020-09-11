2ec9bc58a6838adff792354caf5e4660e0001fc3
ArunBalajiR <arunbalaji25062k@gmail.com>
2020-09-11 17:10:58 +0530

    Password reset Email

--- mybook/settings.py
+++ mybook/settings.py
@@ -162,3 +162,12 @@ LOGIN_URL='login'
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL= '/media/'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL  = os.environ.get('EMAIL_USER')
+print(EMAIL_HOST_USER,EMAIL_HOST_PASSWORD)
